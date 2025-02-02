fd8e1a1752b32db80a5f238993436db8ecadb814
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-02 19:03:31 +0200

    Refactor SessionController to use IMovieService
    Added IMovieService dependency to SessionController.
    Modified constructor to accept and initialize IMovieService.
    Updated Index method to use GetActiveSessionsWithMovies.

--- MVC_Cinema/Controllers/SessionController.cs
+++ MVC_Cinema/Controllers/SessionController.cs
@@ -6,14 +6,17 @@ namespace WebApp.Controllers;
 public class SessionController : Controller
 {
     private readonly ISessionService _sessionService;
+    private readonly IMovieService _movieService;
 
-    public SessionController(ISessionService sessionService)
+    public SessionController(ISessionService sessionService, IMovieService movieService)
     {
         _sessionService = sessionService;
+        _movieService = movieService;
     }
 
     public IActionResult Index()
     {
-        return View(_sessionService.GetAllSessions());
+        var sessionsWithMovies = _sessionService.GetActiveSessionsWithMovies();
+        return View(sessionsWithMovies);
     }
 }
