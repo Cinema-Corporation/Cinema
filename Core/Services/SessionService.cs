dee40df87d862d0f3f26e8509e10fd25b31f0e1d
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-02 19:01:57 +0200

    Add methods to SessionService for active sessions and movies
    Updated SessionService to include IMovieService dependency.
    Added GetActiveSessions method to retrieve active sessions.
    Removed the existing GetAllSessions method.
    Introduced GetActiveSessionsWithMovies to fetch and combine
    session and movie data into SessionWithMovieDTO objects.

--- Core/Services/SessionService.cs
+++ Core/Services/SessionService.cs
@@ -11,15 +11,49 @@ public class SessionService : ISessionService
     private readonly IMapper _mapper;
     private readonly IRepository<Session> _sessionRepository;
 
-    public SessionService(IMapper mapper,IRepository<Session> sessionRepository)
+    private readonly IMovieService _movieService;
+
+    public SessionService(IMapper mapper,
+        IRepository<Session> sessionRepository,
+        IMovieService movieService)
     {
         _mapper = mapper;
         _sessionRepository = sessionRepository;
+        _movieService = movieService;
     }
 
-    public List<SessionDTO> GetAllSessions()
+    public IEnumerable<SessionDTO> GetActiveSessions()
     {
-        var result = _sessionRepository.GetAll();
-        return _mapper.Map<List<SessionDTO>>(result);
+        var activeSessions = _sessionRepository
+            .GetAll()
+            .Where(s => s.TimeEnd > DateTime.Now)
+            .ToList();
+
+        return _mapper.Map<IEnumerable<SessionDTO>>(activeSessions);
+    }
+
+    public IEnumerable<SessionWithMovieDTO> GetActiveSessionsWithMovies()
+    {
+        var activeSessionDTOs = GetActiveSessions();
+
+        var movieIds = activeSessionDTOs
+            .Select(s => s.MovieId)
+            .Distinct()
+            .ToList();
+
+        var movieDTOs = _movieService.GetMoviesByIds(movieIds);
+
+        var result = activeSessionDTOs.Select(sessionDto => 
+            {
+                var movie = movieDTOs.FirstOrDefault(m => m.MovieId == sessionDto.MovieId);
+                return new SessionWithMovieDTO
+                {
+                    Session = sessionDto,
+                    Movie = movie
+                };
+            })
+            .ToList();
+
+        return result;
     }
 }
