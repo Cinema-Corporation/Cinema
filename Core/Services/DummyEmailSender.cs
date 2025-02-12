8262cfa9a68f33e15ae6a68c042ae3a71e5210a1
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-12 16:15:04 +0200

    Add DummyEmailSender class for testing email functionality
    Introduced a new namespace `BusinessLogic.Services` and added a
    `DummyEmailSender` class that implements the `IEmailSender`
    interface from `Microsoft.AspNetCore.Identity.UI.Services`.
    The `SendEmailAsync` method in `DummyEmailSender` provides a
    dummy implementation that returns a completed `Task`, useful
    for testing scenarios where actual email sending is not needed.

---
+++ Core/Services/DummyEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace BusinessLogic.Services
+{
+    public class DummyEmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
