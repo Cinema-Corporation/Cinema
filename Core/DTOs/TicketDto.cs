ef5ad440ac8d4cb1617875ee2e282a879ef4060a
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-17 00:43:09 +0200

    Add UserId property to TicketDTO class
    Updated the TicketDTO class in TicketDto.cs to include a new string property, UserId, initialized to null!. This addition allows the class to store the identifier of the user associated with the ticket.

--- Core/DTOs/TicketDto.cs
+++ Core/DTOs/TicketDto.cs
@@ -5,6 +5,7 @@ public class TicketDTO
     public int TicketId { get; set; }
     public int SessionId { get; set; }
     public int PlaceId { get; set; }
+    public string UserId { get; set; } = null!;
     public decimal Price { get; set; }
     public DateTime PaymentDate { get; set; }
 }
