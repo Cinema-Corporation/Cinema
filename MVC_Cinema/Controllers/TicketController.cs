5dbf2742fb82cdcdfb53ffc07667d76f93d98c95
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-02-17 00:44:00 +0200

    Enhance TicketController with session and ticket services
    - Added ITicketService and ISessionService dependencies.
    - Implemented authorization with [Authorize] attribute.
    - Introduced ChooseSession and ChooseSeat action methods.
    - Added BuySeat and PurchaseSuccess methods for ticket handling.
    - Removed Index method; MyTickets remains unchanged.

--- MVC_Cinema/Controllers/TicketController.cs
+++ MVC_Cinema/Controllers/TicketController.cs
@@ -5,19 +5,67 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using BusinessLogic.Services;
+using Microsoft.AspNetCore.Authorization;
+using BusinessLogic.Interfaces;
+using System.Security.Claims;
 namespace WebApp.Controllers;
 
+[Authorize]
 public class TicketController : Controller
 {
     private readonly AppDbContext _context;
     private readonly UserManager<IdentityUser> _userManager;
 
-    public TicketController(AppDbContext context, UserManager<IdentityUser> userManager)
+    private readonly ITicketService _ticketService;
+    private readonly ISessionService _sessionService;
+
+    public TicketController(AppDbContext context,
+        UserManager<IdentityUser> userManager,
+        ITicketService ticketService,
+        ISessionService sessionService)
     {
         _context = context;
         _userManager = userManager;
+        _ticketService = ticketService;
+        _sessionService = sessionService;
     }
-    public async Task<IActionResult> Index()
+
+    public IActionResult ChooseSession(int movieId)
+    {
+        var sessions = _sessionService.GetActiveSessionsByMovie(movieId);
+        ViewBag.MovieId = movieId;
+        return View(sessions);
+    }
+
+    public IActionResult ChooseSeat(int sessionId)
+    {
+        var availableSeats = _ticketService.GetAvailablePlacementsForSession(sessionId);
+        ViewBag.SessionId = sessionId;
+        return View(availableSeats);
+    }
+
+    [HttpPost]
+    public IActionResult BuySeat(int sessionId, int placeId)
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        var ticket = _ticketService.BuyTicket(sessionId, placeId, userId);
+
+        return RedirectToAction("PurchaseSuccess", new { ticketId = ticket.TicketId });
+    }
+
+    public IActionResult PurchaseSuccess(int ticketId)
+    {
+        var ticket = _ticketService.GetTicketById(ticketId);
+        if (ticket == null)
+        {
+            return NotFound();
+        }
+
+        return View(ticket);
+    }
+
+    public async Task<IActionResult> MyTickets()
     {
         var user = await _userManager.GetUserAsync(User);
         if (user == null)
